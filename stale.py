9cd673a76f45c5754ba5ee7272bbbfb2d6357658
NaveenKundeti-IBS <kundeti.naveen@gmail.com>
2023-03-13 13:36:00 +0530

    stale with wf

--- stale.py
+++ stale.py
@@ -2,8 +2,6 @@ import os
 from github import Github
 from datetime import datetime, timedelta
 
-# access_token = os.environ.get("GITHUB_TOKEN")
-# g = Github(access_token)
 g = Github(os.environ["GITHUB_TOKEN"])
 repo_name = os.environ.get("REPO_NAME")
 pr_number = int(os.environ.get("PR_NUMBER"))
@@ -11,13 +9,18 @@ pr_number = int(os.environ.get("PR_NUMBER"))
 repo = g.get_repo(repo_name)
 pr = repo.get_pull(pr_number)
 pulls = repo.get_pulls(state='open')
+stale_days: int(os.environ.get("STALE_DAYS"))
+stale_close_days: int(os.environ.get("STALE_CLOSE_DAYS"))
 
 print("repo_name:",repo)
-print("pr_number:",pr)
+print("pr_number:",pr.number)
 print("pulls:",pulls)
+print("stale_days:",stale_days)
+print("stale_close_days:",stale_close_days)
+
 
 # define the number of days after which a pull request is considered stale
-stale_days = 15
+stale_no_days = stale_days
 # get the current date
 now = datetime.now()
 # iterate over the pull requests
@@ -31,7 +34,7 @@ for pr in pulls:
         pr.add_to_labels('Stale')
 
 # iterate over each pull request
-stale_close_days = 1
+stale_no_close_days = stale_close_days
 
 for pr in pulls:
     # check if the stale label is applied
