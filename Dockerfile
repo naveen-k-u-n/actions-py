13f41d734d9f6a680aff43df77b84cd1a3ceb991
NaveenKundeti-IBS <kundeti.naveen@gmail.com>
2023-05-02 14:26:10 +0530

    test tag

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,5 @@
 FROM python:3.8-slim-buster
-
-RUN python -m pip install --upgrade pip
-COPY main.py ./
-COPY requirements.txt ./
-RUN python3 -m venv .venv
-RUN pip install -r requirements.txt
-
-CMD ["python", "/main.py"]
-
-
-
-
+WORKDIR /chatops
+COPY . .
+RUN chmod +x script.sh
+CMD cd /chatops && ./script.sh
--- main.py
+++ main.py
@@ -1,58 +1,113 @@
-# import requests
-# import json
 import os
 from github import Github
+from datetime import datetime, timedelta
 
-access_token = os.environ.get("GITHUB_TOKEN")
+access_token = "${{ secrets.GITHUB_TOKEN }}"
+
+# create a Github object using the access token
 g = Github(access_token)
-repo_name = os.environ.get("REPO_NAME")
-pull_number = int(os.environ.get("PR_NUMBER"))
-
-repo = g.get_repo(repo_name)
-pr = repo.get_pull(pull_number)
-
-# values
+# env values
+# g = Github(os.environ["GITHUB_TOKEN"])
+repo = g.get_repo(os.environ['REPO_NAME'])
+pulls = repo.get_pulls(state='open')
 MERGE_PR = os.environ.get("MERGE_PR")
 CLOSE_PR = os.environ.get("CLOSE_PR")
-PR_DESCRIPTION = os.environ.get("PR_DESCRIPTION")
-BASE = os.environ.get("BASE_REF")
-HEAD = os.environ.get("HEAD_REF")
 
+print("repo:",repo)
+print("pulls:",pulls)
+
+# 1.Check if there are any open pull requests
+if pulls.totalCount == 0:
+    print('No open pull requests, exiting...')
+    exit()
+
+# 2.Add "Stale" label to the PR if no active from 15 days
+stale_days = 15
+now = datetime.now()
+for pr in pulls:
+    try:
+        time_diff = now - pr.updated_at
+        # check if the time difference is greater than the stale_days
+        if time_diff > timedelta(days=stale_days):
+            print("Pull request", pr.number, "is stale!")
+            pr.create_issue_comment('This PR is stale because it has been open 15 days with no activity. Remove stale label or comment/update PR otherwise this will be closed in next 2 days.')
+            pr.add_to_labels('Stale')
+    except Exception as e:
+        print('Error occurred while processing pull request:', pr.number)
+        print('Error:', e)
+
+# 3.close staled PR if 2 days of no activity
+stale_close_days = 2
+for pr in pulls:
+    # check if the Stale label is applied on PR
+    if "Stale" in [label.name for label in pr.labels]:
+        try:
+            time_diff = now - pr.updated_at
+            # check if the time difference is greater than the stale_close_days
+            if time_diff > timedelta(days=stale_close_days):
+                print("Pull request", pr.number, "is stale and closed!")
+                pr.edit(state="closed")
+                pr.create_issue_comment('This PR was closed because it has been stalled for 2 days with no activity.')
+        except Exception as e:
+            print('Error occurred while closing pull request:', pr.number)
+            print('Error:', e)
+
+print("pr_updated_at:",pr.updated_at)
+
+# 4.Check if the pull request targets the master branch directly
+for pull in pulls:
+    if pull.base.ref == 'master' and not pull.head.ref.startswith('release/'):
+        try:
+            pull.edit(state='closed')
+            pull.create_issue_comment('Do not accept PR target from feature branch to master branch.')
+            print("Pull request", pull.number, "Do not accept PR target from feature branch to master branch.")
+        except Exception as e:
+            print('Error occurred while processing pull request:', pull.number)
+            print('Error:', e)
+
+# 5.Check if the pull request has a description
+for pull in pulls:
+    if not pull.body:
+        try:
+            pull.edit(state='closed')
+            pull.create_issue_comment('No Description on PR body. Please add valid description.')
+            print("Pull request", pull.number, "No Description on PR body. Please add valid description.")
+        except Exception as e:
+            print('Error occurred while processing pull request:', pull.number)
+            print('Error:', e)
+
+# 6.Check if the Approved or Close comments in the pull request comments
 def merge():
-    print("PR has Approved.")
-    # merge API
-    pr.merge(merge_method = 'merge', commit_message ='Pull Request Approved and Merged!')
-    #  merge API comment
-    pr.create_issue_comment('Pull Request Approved and Merged!')
+    if 'PR_NUMBER' in os.environ:
+        pr_number = int(os.environ['PR_NUMBER'])
+        pr = repo.get_pull(pr_number)
+        print("pr_number:", pr_number)
+        print("pr:", pr)
+        try:
+            pr.create_issue_comment('This pull request was approved and merged because of a slash command.')
+            pr.merge(merge_method = 'merge', commit_message ='Pull Request Approved and Merged!')
+            print("Pull request:", pr, "was approved and merged because of a slash command.")
+        except Exception as e:
+            print(f"Failed to merge PR: {str(e)}")
+            exit()
 
 def close():
-    print("PR has Closed manually by comments.")
-    # closed API
-    pr.edit(state='closed')
-    # closed API comment
-    pr.create_issue_comment('Pull Request Closed!')
-
-def target():
-    # API comment
-    pr.create_issue_comment('Do not accept PR target from feature branch to master branch.')
-    # closed API
-    pr.edit(state='closed')
-
-
-def description():
-    # API comment
-    pr.create_issue_comment('No Description on PR body. Please add valid description.')
-    # closed API
-    pr.edit(state='closed')
+    if 'PR_NUMBER' in os.environ:
+        pr_number = int(os.environ['PR_NUMBER'])
+        pr = repo.get_pull(pr_number)
+        print("pr_number:", pr_number)
+        print("pr:", pr)
+        try:
+            pr.edit(state="closed")
+            pr.create_issue_comment('This pull request was closed because of a slash command.')
+            print("Pull request:", pr, "was closed because of a slash command.")
+        except Exception as e:
+            print(f"Failed to close PR: {str(e)}")
 
 if __name__ == '__main__':
     print('start')
     if MERGE_PR.__eq__('true'):
         merge()  
     if CLOSE_PR.__eq__('true'):
-        close()  
-    if BASE.__eq__('true') and  HEAD.__eq__('false'):
-        target() 
-    if PR_DESCRIPTION.__eq__('true'):
-        description() 
-    print('end')
+        close()   
+    print('end')      
